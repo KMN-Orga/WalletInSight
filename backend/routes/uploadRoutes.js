de9652a795f758237bdeeae748f16a76fcfa59f5
Giane <giane.teo@gmail.com>
2025-03-20 14:15:17 +0100

    feat: change be to nest.js

--- backend/routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import * as uploadController from '../controllers/uploadController.js';
-
-const router = express.Router();
-
-router.get('/', uploadController.getUpload);
-
-export default router;
